0c1e08149aa8b40677c5f5af47378e4d563fc627
Fabio Lima <silva.l3@hotmail.com>
2026-05-09 20:42:06 -0300

    wip:

---
+++ sorveteria-rai/sorveteria-rai/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace sorveteria_rai.Models
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        public string NomeCategoria { get; set; }
+        public string DescricaoCategoria { get; set; }
+    }
+}
---
+++ sorveteria-rai/sorveteria-rai/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sorveteria_rai.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
